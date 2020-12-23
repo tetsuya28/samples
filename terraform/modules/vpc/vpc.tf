f98e48d046baaa05a4c635015aa368e25adf720e
tetsuya28 <tet.lv11@gmail.com>
2020-12-23 10:14:10 +0900

    :bug: Fix vairable name
    vpc_cidr_block to vpc_cidr

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "public" {
   count  = var.subnet_count
   vpc_id = aws_vpc.this.id
   # + one octet based on vpc cidr range
-  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
   availability_zone       = data.aws_availability_zones.this.names[count.index]
   map_public_ip_on_launch = true
   tags = {
@@ -27,7 +27,7 @@ resource "aws_subnet" "private" {
   count  = var.subnet_count
   vpc_id = aws_vpc.this.id
   # + one octet based on vpc cidr range
-  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
   availability_zone       = data.aws_availability_zones.this.names[count.index]
   map_public_ip_on_launch = false
   tags = {
