57e0e716f30132b7f68df32970bd149ebf656e7c
tetsuya28 <tet.lv11@gmail.com>
2020-12-23 10:26:24 +0900

    :bug: Fix private subnet route_table

--- terraform/modules/vpc/route_table.tf
+++ terraform/modules/vpc/route_table.tf
@@ -12,9 +12,13 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
+  # set route if nat exists
+  dynamic "route" {
+    for_each = var.nat ? [""] : []
+    content {
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.this[0].id
+    }
   }
   tags = {
     Name      = "${var.app}-private"
@@ -29,7 +33,7 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_route_table_association" "private" {
-  count          = var.nat ? var.subnet_count : 0
+  count          = var.subnet_count
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
