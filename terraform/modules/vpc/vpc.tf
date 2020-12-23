0b79f7a3e2fbe14ba010328e2e14cad085de2a29
tetsuya28 <tet.lv11@gmail.com>
2020-12-23 10:26:47 +0900

    :bug: Fix use different subnet_cidr between public and private

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -27,7 +27,7 @@ resource "aws_subnet" "private" {
   count  = var.subnet_count
   vpc_id = aws_vpc.this.id
   # + one octet based on vpc cidr range
-  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index + 100)
   availability_zone       = data.aws_availability_zones.this.names[count.index]
   map_public_ip_on_launch = false
   tags = {
