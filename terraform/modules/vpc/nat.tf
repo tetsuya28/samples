87951f7fb2cef8526f3e2c129503e8f3283fe129
tetsuya28 <tet.lv11@gmail.com>
2020-12-29 00:50:24 +0900

    :art: fix vpc nat

--- terraform/modules/vpc/nat.tf
+++ terraform/modules/vpc/nat.tf
@@ -1,5 +1,6 @@
 resource "aws_eip" "this" {
-  vpc = true
+  count = var.nat ? 1 : 0
+  vpc   = true
   tags = {
     Name      = "${var.app}-nat"
     CreatedBy = var.created_by
@@ -8,7 +9,7 @@ resource "aws_eip" "this" {
 
 resource "aws_nat_gateway" "this" {
   count         = var.nat ? 1 : 0
-  allocation_id = aws_eip.this.id
+  allocation_id = aws_eip.this[0].id
   subnet_id     = aws_subnet.public[0].id
   tags = {
     Name      = var.app
