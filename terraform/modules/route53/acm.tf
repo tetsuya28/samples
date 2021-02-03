b5c8b897a59b535dec37885a7e34d03e154eb9a4
tetsuya28 <tet.lv11@gmail.com>
2021-02-03 11:14:36 +0900

    :art: add acm_certificate in route53 module

---
+++ terraform/modules/route53/acm.tf
@@ -0,0 +1,33 @@
+resource "aws_acm_certificate" "this" {
+  domain_name               = var.domain
+  subject_alternative_names = ["*.${var.domain}"]
+  validation_method         = "DNS"
+  tags = {
+    Name      = "${var.app}-${var.env}"
+    Env       = var.env
+    CreatedBy = var.created_by
+  }
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn         = aws_acm_certificate.this.arn
+  validation_record_fqdns = [for record in aws_route53_record.this : record.fqdn]
+}
+
+resource "aws_route53_record" "this" {
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+      zone_id = aws_route53_zone.this.zone_id
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
+}
--- terraform/modules/route53/output.tf
+++ terraform/modules/route53/output.tf
@@ -1,3 +1,7 @@
 output "zone_id" {
   value = aws_route53_zone.this.zone_id
 }
+
+output "acm_arn" {
+  value = aws_acm_certificate.this.arn
+}
